c8ca45ac233a7a307079d6cd58cde2fb4e51ece3
Exxon-Dev <ekosulistyo952@gmail.com>
2025-07-17 12:05:01 +0700

    memperbaiki sweetalert2 pada halaman dashboard_pembimbing.php

--- absensi/pages/sign-up_aksi.php
+++ absensi/pages/sign-up_aksi.php
@@ -1,6 +1,13 @@
 <?php
- unset($_SESSION['username']);
- unset($_SESSION['login']);
- session_destroy();
- header("Location: sign-in.php");
+session_start(); // Pastikan session_start() ada di awal
+unset($_SESSION['username']);
+unset($_SESSION['login']);
+session_destroy();
+
+// Hapus localStorage dan redirect
+echo '<script>
+    localStorage.removeItem("welcomeAlertShown");
+    window.location.href = "sign-in.php";
+</script>';
+exit();
 ?>
