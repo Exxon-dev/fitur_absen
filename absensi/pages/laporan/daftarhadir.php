ccfe7d8123e7941ae2199b59846bdeac806e7003
nadevs <hasnanur.p25@gmail.com>
2025-08-20 08:44:44 +0700

    mengubah letak tanda tangan

--- absensi/pages/laporan/daftarhadir.php
+++ absensi/pages/laporan/daftarhadir.php
@@ -7,215 +7,155 @@ if (!$id_siswa) {
     exit();
 }
 
+// ambil data siswa + pembimbing
 $query_siswa = "
     SELECT 
         s.nama_siswa, 
-        s.nisn
-    FROM 
-        siswa s 
-    WHERE 
-        s.id_siswa = ?
+        s.nisn, 
+        s.pro_keahlian AS jurusan, 
+        p.nama_pembimbing
+    FROM siswa s
+    LEFT JOIN pembimbing p ON s.id_pembimbing = p.id_pembimbing
+    WHERE s.id_siswa = ?
 ";
-
 $stmt_siswa = $coneksi->prepare($query_siswa);
 $stmt_siswa->bind_param("i", $id_siswa);
 $stmt_siswa->execute();
 $result_siswa = $stmt_siswa->get_result();
+$data_siswa = $result_siswa->fetch_assoc();
 
-if ($result_siswa->num_rows > 0) {
-    $data_siswa = $result_siswa->fetch_assoc();
-    $nama_siswa = htmlspecialchars($data_siswa['nama_siswa']);
-    $nisn = htmlspecialchars($data_siswa['nisn']);
-} else {
-    echo "Data siswa tidak ditemukan.";
-    exit();
-}
+$nama_siswa      = $data_siswa['nama_siswa'] ?? '';
+$nisn            = $data_siswa['nisn'] ?? '';
+$jurusan         = $data_siswa['jurusan'] ?? '';
+$nama_pembimbing = $data_siswa['nama_pembimbing'] ?? '........................';
 
-$query_absen = "
-    SELECT 
-        a.tanggal, 
-        a.jam_masuk, 
-        a.jam_keluar, 
-        a.keterangan 
-    FROM 
-        absen a 
-    WHERE 
-        a.id_siswa = ? 
-    ORDER BY a.tanggal
+// ambil data jurnal + catatan
+$query_jurnal = "
+    SELECT j.tanggal, j.keterangan, c.catatan
+    FROM jurnal j
+    LEFT JOIN catatan c ON j.id_jurnal = c.id_jurnal
+    WHERE j.id_siswa = ? 
+    ORDER BY j.tanggal ASC
 ";
+$stmt_jurnal = $coneksi->prepare($query_jurnal);
+$stmt_jurnal->bind_param("i", $id_siswa);
+$stmt_jurnal->execute();
+$result_jurnal = $stmt_jurnal->get_result();
 
-$stmt_absen = $coneksi->prepare($query_absen);
-$stmt_absen->bind_param("i", $id_siswa);
-$stmt_absen->execute();
-$result_absen = $stmt_absen->get_result();
-
-$kehadiran = [];
-while ($row = $result_absen->fetch_assoc()) {
-    $kehadiran[] = $row;
+$jurnal_data = [];
+while ($row = $result_jurnal->fetch_assoc()) {
+    $jurnal_data[] = $row;
 }
 
-$stmt_siswa->close();
-$stmt_absen->close();
-$coneksi->close();
+// group per bulan
+$grouped_by_month = [];
+foreach ($jurnal_data as $row) {
+    $bulan = date("Y-m", strtotime($row['tanggal']));
+    $grouped_by_month[$bulan][] = $row;
+}
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
-
+<html lang="id">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Daftar Hadir Praktik Kerja Lapangan</title>
-    <style type="text/css">
-        @page {
-            size: A4;
-            margin: 20mm;
-        }
-
-        .printable {
-            margin: 20px;
-        }
-
-        @media print {
-            .no-print {
-                display: none;
-            }
-        }
-
-        .style6 {
-            font-family: Verdana, Arial, Helvetica, sans-serif;
-            font-size: 16px;
-        }
-
-        .style9 {
+    <title>Jurnal Prakerin</title>
+    <style>
+        body {
             font-family: Verdana, Arial, Helvetica, sans-serif;
             font-size: 11px;
         }
-
-        .style27 {
-            font-family: Verdana, Arial, Helvetica, sans-serif;
-            font-size: 11px;
-            font-weight: bold;
-        }
-
-        .top {
-            border-top: 1px solid #000000;
-        }
-
-        .bottom {
-            border-bottom: 1px solid #000000;
-        }
-
-        .left {
-            border-left: 1px solid #000000;
-        }
-
-        .right {
-            border-right: 1px solid #000000;
-        }
-
-        .all {
-            border: 1px solid #000000;
-        }
-
-        .align-justify {
-            text-align: justify;
-        }
-
-        .align-center {
+        .judul {
             text-align: center;
+            font-weight: bold;
+            font-size: 11px;
         }
-
-        .align-right {
+        table {
+            border-collapse: collapse;
+            width: 100%;
+            margin-top: 10px;
+        }
+        th, td {
+            border: 1px solid #000;
+            padding: 6px;
+            font-size: 11px;
+        }
+        .ttd {
+            margin-top: 100px; /* kasih jarak lebih luas */
+            width: 100%;
+            text-align: right;
+            font-size: 11px;
+        }
+        .ttd div {
             text-align: right;
         }
-
-        /* Atur alignment tiap kolom tabel */
-        table td:nth-child(1),
-        table th:nth-child(1) {
-            text-align: center;
-        }
-
-        table td:nth-child(2),
-        table th:nth-child(2) {
-            text-align: left;
-            padding-left: 5px;
-        }
-
-        table td:nth-child(3),
-        table td:nth-child(4),
-        table th:nth-child(3),
-        table th:nth-child(4) {
-            text-align: center;
-        }
-
-        table td:nth-child(5),
-        table th:nth-child(5) {
-            text-align: left;
-            padding-left: 5px;
+        .page-break {
+            page-break-after: always;
         }
     </style>
     <script>
         function printReport() {
             window.print();
         }
-
         window.onload = function() {
             printReport();
         };
     </script>
-    <script src="https://cdn.tailwindcss.com"></script>
 </head>
-
 <body>
-    <div class="printable">
-        <div class="text-center mb-8">
-            <p class="font-bold style9">DAFTAR HADIR PESERTA DIDIK PRAKTIK KERJA LAPANGAN</p>
-        </div>
-        <table cellspacing="2">
-            <div class="style9 mb-4">
-                <p>NAMA: <?php echo $nama_siswa; ?></p>
-                <p>NISN: <?php echo $nisn; ?></p>
-            </div>
-        </table>
-        <table width="96%" border="1" align="center" cellpadding="3" cellspacing="0" class="style9">
-            <thead>
-                <tr height="35">
-                    <th class="left bottom top">No</th>
-                    <th class="left bottom top">Hari/Tanggal</th>
-                    <th class="left bottom top">MASUK</th>
-                    <th class="left bottom top">PULANG</th>
-                    <th class="left bottom top right">Keterangan</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                $no = 1;
-                foreach ($kehadiran as $record) {
-                    echo "<tr height='25' class='style27'>
-                        <td class='top bottom left text-center'>{$no}</td>
-                        <td class='top bottom left text-center'>" . htmlspecialchars($record['tanggal']) . "</td>
-                        <td class='top bottom left text-center'>" . htmlspecialchars($record['jam_masuk']) . "</td>
-                        <td class='top bottom left text-center'>" . htmlspecialchars($record['jam_keluar'] ?? '') . "</td>
-                        <td class='top bottom left right text-center'>" . htmlspecialchars($record['keterangan']) . "</td>
-                    </tr>";
-                    $no++;
-                }
-                ?>
-                <tr>
-                    <td colspan="5">&nbsp;</td>
-                </tr>
-            </tbody>
-        </table>
-        <div class="flex justify-end items-center mb-8">
-            <div class="text-center">
-                <div class="style9">Kajoran, ..................... 202...</div>
-                <div class="style9">PEMBIMBING DUDI</div>
-                <br><br><br>
-                <div class="style9">(.............................................)</div>
-            </div>
+
+<?php foreach ($grouped_by_month as $bulan => $records): ?>
+    <div class="judul">
+        <div>JURNAL PRAKERIN</div>
+        <div>SMA N 1 Magelang</div>
+        <div>TAHUN AJARAN 2025/2026</div>
+    </div>
+
+    <br>
+    <p>Nama : <?= htmlspecialchars($nama_siswa) ?></p>
+    <p>NISN : <?= htmlspecialchars($nisn) ?></p>
+    <p>Jurusan : <?= htmlspecialchars($jurusan) ?></p>
+
+    <table>
+        <tr>
+            <th>No</th>
+            <th>Hari/Tanggal</th>
+            <th>Uraian Kegiatan/Uraian Materi</th>
+            <th>Catatan</th>
+            <th>Tanda Tangan</th>
+        </tr>
+        <?php $no=1; foreach ($records as $row): ?>
+            <tr>
+                <td><?= $no++ ?></td>
+                <td><?= date("d-m-Y", strtotime($row['tanggal'])) ?></td>
+                <td><?= htmlspecialchars($row['keterangan']) ?></td>
+                <td><?= htmlspecialchars($row['catatan'] ?? '') ?></td>
+                <td></td>
+            </tr>
+        <?php endforeach; ?>
+
+        <?php 
+        // tambahin baris kosong biar 7 per halaman
+        $remaining = 7 - count($records);
+        for ($i=0; $i < $remaining; $i++): ?>
+            <tr> 
+                <td>&nbsp;</td><td></td><td></td><td></td><td></td>
+            </tr>
+        <?php endfor; ?>
+    </table>
+
+    <!-- tanda tangan -->
+    <div class="ttd">
+        <div>
+            <div>....................., ..................... 202...</div>
+            <div style="margin-top:20px;">PEMBIMBING DUDI</div>
+            <br><br><br><br><br><br>
+            <div>(<?= htmlspecialchars($nama_pembimbing) ?>)</div>
         </div>
     </div>
-</body>
 
+    <div class="page-break"></div>
+<?php endforeach; ?>
+
+</body>
 </html>
