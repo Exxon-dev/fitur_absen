ebf9ead288abdbe467ca1bb473cb4243e6c3478e
Exxon-Dev <ekosulistyo952@gmail.com>
2025-08-21 08:45:31 +0700

    mengganti jam pada catatan

--- absensi/pages/catatan/catatan.php
+++ absensi/pages/catatan/catatan.php
@@ -38,7 +38,7 @@ if ($current_day == 7) { // Hari Minggu
         $allow_jurnal = ($current_time >= '11:00' && $current_time <= '12:15');
         $time_message = 'Jurnal hanya bisa ditambahkan/diupdate antara jam 11.00 - 12.15 pada hari Sabtu';
     } else { // Hari Senin-Jumat
-        $allow_jurnal = ($current_time >= '15:00' && $current_time <= '23:15');
+        $allow_jurnal = ($current_time >= '15:00' && $current_time <= '16:15');
         $time_message = 'Jurnal hanya bisa ditambahkan/diupdate antara jam 15.00 - 16.15 pada hari Senin-Jumat';
     }
 }
